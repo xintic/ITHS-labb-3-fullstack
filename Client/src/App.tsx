e239cc5600305b3adac231cb96fc1aa3659dba55
xintic <oskar.sjobeck@gmail.com>
2025-05-21 19:11:54 +0200

    Started routing.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,23 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import MainLayout from './layouts/MainLayout';
 import './index.css';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<LandingPage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
